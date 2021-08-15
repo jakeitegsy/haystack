c3881aa14eb67880cd7fd58ddb392a840ece02ab
jakeitegsy@yahoo.com <jakeitegsy@yahoo.com>
2021-08-14 23:53:55 -0500

    replace variables with functions

--- tests/test_stockpup_analyst.py
+++ tests/test_stockpup_analyst.py
@@ -10,12 +10,13 @@ from pandas import Index, MultiIndex, concat
 analyst = Analyst(source='STOCKPUP', ticker='BAC').get_stock()
 raw_data = analyst.get_raw_data()
 
-
 class TestStockPup(unittest.TestCase):
 
 	def test_source_folder_for_stockpup_data(self):
 		self.assertEqual(
-			Analyst(source='stockpup').get_stock().source_folder(), 
+			Analyst(
+				source='stockpup'
+			).get_stock().source_folder(), 
 			'stockpup_data'
 		)
 
@@ -186,34 +187,62 @@ class TestStockPup(unittest.TestCase):
 	        ], names=['YEAR', 'QUARTER'])
 		)
 
+	def annual_data(self):
+		return analyst.get_annual_data(raw_data)
+
 	def test_get_annual_data_drops_years_without_4_quarters(self):
-		annual_data = analyst.get_annual_data(raw_data)
-		for year in annual_data.index.levels[0]:
+		for year in self.annual_data().index.levels[0]:
 			with self.subTest(i=year):
 				try:
-					self.assertEqual(len(annual_data[year], 4))
+					self.assertEqual(len(self.annual_data()[year], 4))
 				except KeyError:
 					continue
 
 	def test_replace_null_values_with_zero(self):
 		self.assertFalse(
-			analyst.replace_null_values_with_zero(raw_data).isna().values.any()
+			(
+				analyst
+					.replace_null_values_with_zero(
+						raw_data
+					)
+				.isna()
+				.values.any()
+			)
 		)
 
 	def test_convert_2000_new_year_to_1999_year_end(self):
  		self.assertFalse(
- 			analyst.convert_2000_new_year_to_1999_year_end(raw_data).loc['2000-01-01'].values.any()
+ 			(
+				analyst
+			 		.convert_2000_new_year_to_1999_year_end(
+						raw_data
+					)
+			)
+			.loc['2000-01-01']
+			.values.any()
 		)
 	
 	def test_set_uppercase_column_names(self):
 		assert_index_equal(
-			analyst.set_uppercase_column_names(raw_data).columns,
-			raw_data.rename(str.upper, axis='columns').columns
+			(
+				analyst
+					.set_uppercase_column_names(raw_data)
+					.columns
+			),
+			(
+				raw_data
+					.rename(str.upper, axis='columns')
+					.columns
+			)
 		)
 
 	def test_rename_columns_changes_column_names(self):
 		assert_index_equal(
-			analyst.rename_columns(raw_data).columns,
+			(
+				analyst
+					.rename_columns(raw_data)
+					.columns
+			),
 			Index([
 				'NET_SHARES_NO_SPLIT',
 				'NET_SHARES',
@@ -260,268 +289,372 @@ class TestStockPup(unittest.TestCase):
 
 	def test_set_numeric_datatypes(self):
 		self.assertTrue(
-			analyst.set_numeric_datatypes(raw_data).dtypes.apply(is_numeric_dtype).all(),
+			(
+				analyst
+					.set_numeric_datatypes(raw_data)
+					.dtypes.apply(is_numeric_dtype)
+					.all()
+			)
+		)
+
+	def net_columns(self):
+		return (
+			'CURRENT_ASSETS',
+			'CURRENT_LIABILITIES',
+			'NET_ASSETS',
+			'NET_CASH',
+			'NET_CASH_FIN',
+			'NET_CASH_INVESTED',
+			'NET_CASH_OP',
+			'NET_DEBT',
+			'NET_DIVIDENDS',
+			'NET_EQUITY',
+			'NET_EXPENSES',
+			'NET_FCF',
+			'NET_FCF_SHY',
+			'NET_GOODWILL',
+			'NET_INCOME',
+			'NET_INVESTED_CAPITAL',
+			'NET_LIABILITIES',
+			'NET_NONCONTROLLING',
+			'NET_RETAINED',
+			'NET_REVENUE',
+			'NET_SHARES',
+			'NET_TANGIBLE',
+			'NET_WORKING_CAPITAL'
 		)
 
 	def test_calculate_net_values(self):
 		assert_index_equal(
-			analyst.calculate_net_values(raw_data).columns,
-			Index([
-				'CURRENT_ASSETS',
-				'CURRENT_LIABILITIES',
-				'NET_ASSETS',
-				'NET_CASH',
-				'NET_CASH_FIN',
-				'NET_CASH_INVESTED',
-				'NET_CASH_OP',
-				'NET_DEBT',
-				'NET_DIVIDENDS',
-				'NET_EQUITY',
-				'NET_EXPENSES',
-				'NET_FCF',
-				'NET_FCF_SHY',
-				'NET_GOODWILL',
-				'NET_INCOME',
-				'NET_INVESTED_CAPITAL',
-				'NET_LIABILITIES',
-				'NET_NONCONTROLLING',
-				'NET_RETAINED',
-				'NET_REVENUE',
-				'NET_SHARES',
-				'NET_TANGIBLE',
-				'NET_WORKING_CAPITAL'
-   			])
+			(
+				analyst
+					.calculate_net_values(
+					raw_data
+				)
+				.columns
+			),
+			Index(self.net_columns())
 		)
 
 	def test_aggregate_keeps_same_columns_as_net_calculations(self):
 		assert_index_equal(
-			analyst.aggregate_quarters_to_years(raw_data).columns,
-			analyst.calculate_net_values(raw_data).columns
+			(
+				analyst
+					.aggregate_quarters_to_years(
+						raw_data
+					)
+					.columns
+			),
+			(
+				analyst
+					.calculate_net_values(
+						raw_data
+						)
+					.columns
+			)
 		)
 
 	def test_moving_averages_keep_same_columns_as_net_calculations(self):
 		assert_index_equal(
-			analyst.calculate_moving_averages(raw_data).columns,
-			analyst.calculate_net_values(raw_data).columns
+			(
+				analyst
+					.calculate_moving_averages(raw_data)
+					.columns
+			),
+			(
+				analyst
+					.calculate_net_values(raw_data)
+				.columns
+			)
 		)
 
 	def test_calculate_moving_average_differences(self):
 		assert_index_equal(
-			analyst.calculate_moving_average_differences(raw_data).columns,
+			(
+				analyst
+					.calculate_moving_average_differences(
+						raw_data
+					)
+					.columns
+			),
 			Index([
-				'DIFF_ASSETS_DEBT',
-				'DIFF_ASSETS_LIABILITIES',
-				'DIFF_CASH_DEBT',
-				'DIFF_CASH_LIABILITIES',
-				'DIFF_CURRENT_ASSETS_DEBT',
-				'DIFF_CURRENT_ASSETS_LIABILITIES',
-				'DIFF_EQUITY_DEBT',
-				'DIFF_EQUITY_LIABILITIES',
-				'DIFF_FCF_DEBT',
-				'DIFF_FCF_LIABILITIES',
-				'DIFF_FCF_SHY_DEBT',
-				'DIFF_FCF_SHY_LIABILITIES',
-				'DIFF_INCOME_DEBT',
-				'DIFF_INCOME_LIABILITIES',
-				'DIFF_TANGIBLE_DEBT',
-				'DIFF_TANGIBLE_LIABILITIES'
-			])
+				f'DIFF_{column}' for column in (
+					'ASSETS_DEBT',
+					'ASSETS_LIABILITIES',
+					'CASH_DEBT',
+					'CASH_LIABILITIES',
+					'CURRENT_ASSETS_DEBT',
+					'CURRENT_ASSETS_LIABILITIES',
+					'EQUITY_DEBT',
+					'EQUITY_LIABILITIES',
+					'FCF_DEBT',
+					'FCF_LIABILITIES',
+					'FCF_SHY_DEBT',
+					'FCF_SHY_LIABILITIES',
+					'INCOME_DEBT',
+					'INCOME_LIABILITIES',
+					'TANGIBLE_DEBT',
+					'TANGIBLE_LIABILITIES'
+				)
+			])			
 		)
 
 	def test_calculate_growth_rates(self):
 		assert_index_equal(
-			analyst.calculate_moving_average_growth_rates(raw_data).index,
+			(
+				analyst
+					.calculate_moving_average_growth_rates(
+						raw_data
+					)
+				.index
+			),
 			Index([
-				'GROWTH_CURRENT_ASSETS',
-                'GROWTH_CURRENT_LIABILITIES',
-                'GROWTH_NET_ASSETS',
-                'GROWTH_NET_CASH',
-                'GROWTH_NET_CASH_FIN',
-                'GROWTH_NET_CASH_INVESTED',
-                'GROWTH_NET_CASH_OP',
-                'GROWTH_NET_DEBT',
-                'GROWTH_NET_DIVIDENDS',
-                'GROWTH_NET_EQUITY',
-                'GROWTH_NET_EXPENSES',
-                'GROWTH_NET_FCF',
-                'GROWTH_NET_FCF_SHY',
-                'GROWTH_NET_GOODWILL',
-                'GROWTH_NET_INCOME',
-                'GROWTH_NET_INVESTED_CAPITAL',
-                'GROWTH_NET_LIABILITIES',
-                'GROWTH_NET_NONCONTROLLING',
-                'GROWTH_NET_RETAINED',
-                'GROWTH_NET_REVENUE',
-                'GROWTH_NET_SHARES',
-                'GROWTH_NET_TANGIBLE',
-                'GROWTH_NET_WORKING_CAPITAL',
+				f'GROWTH_{column}' 
+				for column in self.net_columns()
 			])
 		)
 
 	def test_calculate_moving_average_ratios(self):
 		assert_index_equal(
-			analyst.calculate_moving_average_ratios(raw_data).columns,
+			(
+				analyst
+					.calculate_moving_average_ratios(
+						raw_data
+					)
+					.columns
+			),
 			Index([
-				'RATIO_CASH_ASSETS',
-				'RATIO_CASH_DEBT',
-				'RATIO_CASH_LIABILITIES',
-				'RATIO_CASH_TANGIBLE',
-				'RATIO_CURRENT',
-				'RATIO_CURRENT_ASSETS_LIABILITIES',
-				'RATIO_CURRENT_DEBT',
-				'RATIO_EQUITY_ASSETS',
-				'RATIO_EQUITY_DEBT',
-				'RATIO_EQUITY_LIABILITIES',
-				'RATIO_EQUITY_TANGIBLE',
-				'RATIO_EXPENSES_REVENUE',
-				'RATIO_FCF_ASSETS',
-				'RATIO_FCF_DEBT',
-				'RATIO_FCF_EQUITY',
-				'RATIO_FCF_EXPENSES',
-				'RATIO_FCF_INVESTED_CAPITAL',
-				'RATIO_FCF_LIABILITIES',
-				'RATIO_FCF_TANGIBLE',
-				'RATIO_FCF_SHY_ASSETS',
-				'RATIO_FCF_SHY_DEBT',
-				'RATIO_FCF_SHY_EQUITY',
-				'RATIO_FCF_SHY_EXPENSES',
-				'RATIO_FCF_SHY_INVESTED_CAPITAL',
-				'RATIO_FCF_SHY_LIABILITIES',
-				'RATIO_FCF_SHY_TANGIBLE',
-				'RATIO_INCOME_ASSETS',
-				'RATIO_INCOME_DEBT',
-				'RATIO_INCOME_EQUITY',
-				'RATIO_INCOME_EXPENSES',
-				'RATIO_INCOME_INVESTED_CAPITAL',
-				'RATIO_INCOME_LIABILITIES',
-				'RATIO_INCOME_TANGIBLE',
-				'RATIO_TANGIBLE_ASSETS',
-				'RATIO_TANGIBLE_LIABILITIES',
-				'RATIO_TANGIBLE_DEBT'
+				f'RATIO_{column}' for column in (
+					'CASH_ASSETS',
+					'CASH_DEBT',
+					'CASH_LIABILITIES',
+					'CASH_TANGIBLE',
+					'CURRENT',
+					'CURRENT_ASSETS_LIABILITIES',
+					'CURRENT_DEBT',
+					'EQUITY_ASSETS',
+					'EQUITY_DEBT',
+					'EQUITY_LIABILITIES',
+					'EQUITY_TANGIBLE',
+					'EXPENSES_REVENUE',
+					'FCF_ASSETS',
+					'FCF_DEBT',
+					'FCF_EQUITY',
+					'FCF_EXPENSES',
+					'FCF_INVESTED_CAPITAL',
+					'FCF_LIABILITIES',
+					'FCF_TANGIBLE',
+					'FCF_SHY_ASSETS',
+					'FCF_SHY_DEBT',
+					'FCF_SHY_EQUITY',
+					'FCF_SHY_EXPENSES',
+					'FCF_SHY_INVESTED_CAPITAL',
+					'FCF_SHY_LIABILITIES',
+					'FCF_SHY_TANGIBLE',
+					'INCOME_ASSETS',
+					'INCOME_DEBT',
+					'INCOME_EQUITY',
+					'INCOME_EXPENSES',
+					'INCOME_INVESTED_CAPITAL',
+					'INCOME_LIABILITIES',
+					'INCOME_TANGIBLE',
+					'TANGIBLE_ASSETS',
+					'TANGIBLE_LIABILITIES',
+					'TANGIBLE_DEBT'
+				)
 			])
 		)
 
 	def test_calculate_moving_averages_per_share(self):
 		assert_index_equal(
-			analyst.calculate_moving_averages_per_share(raw_data).columns,
+			(
+				analyst
+					.calculate_moving_averages_per_share(
+						raw_data
+					)
+					.columns
+			),
 			Index([
-				'PER_SHARE_CURRENT_ASSETS',
-				'PER_SHARE_CURRENT_LIABILITIES',
-				'PER_SHARE_NET_ASSETS',
-				'PER_SHARE_NET_CASH',
-				'PER_SHARE_NET_CASH_FIN',
-				'PER_SHARE_NET_CASH_INVESTED',
-				'PER_SHARE_NET_CASH_OP',
-				'PER_SHARE_NET_DEBT',
-				'PER_SHARE_NET_DIVIDENDS',
-				'PER_SHARE_NET_EQUITY',
-				'PER_SHARE_NET_EXPENSES',
-				'PER_SHARE_NET_FCF',
-				'PER_SHARE_NET_FCF_SHY',
-				'PER_SHARE_NET_GOODWILL',
-				'PER_SHARE_NET_INCOME',
-				'PER_SHARE_NET_INVESTED_CAPITAL',
-				'PER_SHARE_NET_LIABILITIES',
-				'PER_SHARE_NET_NONCONTROLLING',
-				'PER_SHARE_NET_RETAINED',
-				'PER_SHARE_NET_REVENUE',
-				'PER_SHARE_NET_SHARES',
-				'PER_SHARE_NET_TANGIBLE',
-				'PER_SHARE_NET_WORKING_CAPITAL',
-				'PER_SHARE_DCF_HISTORIC',
-				'PER_SHARE_DCF_SHY_HISTORIC'
+				f'PER_SHARE_{column}' for column in (
+					'CURRENT_ASSETS',
+					'CURRENT_LIABILITIES',
+					'NET_ASSETS',
+					'NET_CASH',
+					'NET_CASH_FIN',
+					'NET_CASH_INVESTED',
+					'NET_CASH_OP',
+					'NET_DEBT',
+					'NET_DIVIDENDS',
+					'NET_EQUITY',
+					'NET_EXPENSES',
+					'NET_FCF',
+					'NET_FCF_SHY',
+					'NET_GOODWILL',
+					'NET_INCOME',
+					'NET_INVESTED_CAPITAL',
+					'NET_LIABILITIES',
+					'NET_NONCONTROLLING',
+					'NET_RETAINED',
+					'NET_REVENUE',
+					'NET_SHARES',
+					'NET_TANGIBLE',
+					'NET_WORKING_CAPITAL',
+					'DCF_HISTORIC',
+					'DCF_SHY_HISTORIC'
+				)
 			])
 		)
 
 	def test_calculate_average_per_share_averages(self):
 		assert_index_equal(
-			analyst.calculate_average_per_share_averages(raw_data).index,
-			analyst.calculate_moving_averages_per_share(raw_data).columns
+			(
+				analyst
+					.calculate_average_per_share_averages(
+						raw_data
+					)
+					.index
+			),
+			(
+				analyst
+					.calculate_moving_averages_per_share(
+						raw_data
+					)
+				.columns
+			)
 		)
 
 	def test_calculate_average_moving_averages(self):
 		assert_index_equal(
-			analyst.calculate_average_moving_averages(raw_data).index,
-			analyst.calculate_moving_averages(raw_data).columns
+			(
+				analyst
+					.calculate_average_moving_averages(
+						raw_data
+					)
+					.index
+			),
+			(
+				analyst
+					.calculate_moving_averages(
+						raw_data
+					)
+					.columns
+			)
 		)
 
 	def test_average_moving_average_differences(self):
 		assert_index_equal(
-			analyst.calculate_moving_average_differences(raw_data).columns,
-			analyst.calculate_average_moving_average_differences(raw_data).index
+			(
+				analyst
+					.calculate_moving_average_differences(
+						raw_data
+					)
+					.columns
+			),
+			(
+				analyst
+					.calculate_average_moving_average_differences(raw_data)
+					.index
+			)
 		)
 
 	def test_average_moving_average_ratios(self):
 		assert_index_equal(
-			analyst.calculate_average_moving_average_ratios(raw_data).index,
-			analyst.calculate_moving_average_ratios(raw_data).columns
+			(
+				analyst
+					.calculate_average_moving_average_ratios(
+						raw_data
+					)
+					.index
+			),
+			(
+				analyst
+					.calculate_moving_average_ratios(
+						raw_data
+					)
+				.columns
+			)
 		)
 
 	def test_average_per_share_differences(self):
 		assert_index_equal(
-			analyst.calculate_average_per_share_differences(raw_data).index,
+			(
+				analyst
+					.calculate_average_per_share_differences(
+						raw_data
+					)
+				.index
+			),
 			Index([
-				'PER_SHARE_DIFF_ASSETS_DEBT',
-				'PER_SHARE_DIFF_ASSETS_LIABILITIES',
-				'PER_SHARE_DIFF_CASH_DEBT',
-				'PER_SHARE_DIFF_CASH_LIABILITIES',
-				'PER_SHARE_DIFF_CURRENT_ASSETS_DEBT',
-				'PER_SHARE_DIFF_CURRENT_ASSETS_LIABILITIES',
-				'PER_SHARE_DIFF_EQUITY_DEBT',
-				'PER_SHARE_DIFF_EQUITY_LIABILITIES',
-				'PER_SHARE_DIFF_FCF_DEBT',
-				'PER_SHARE_DIFF_FCF_LIABILITIES',
-				'PER_SHARE_DIFF_FCF_SHY_DEBT',
-				'PER_SHARE_DIFF_FCF_SHY_LIABILITIES',
-				'PER_SHARE_DIFF_INCOME_DEBT',
-				'PER_SHARE_DIFF_INCOME_LIABILITIES',
-				'PER_SHARE_DIFF_TANGIBLE_DEBT',
-				'PER_SHARE_DIFF_TANGIBLE_LIABILITIES'
+				f'PER_SHARE_DIFF_{column}' for column in (
+					'ASSETS_DEBT',
+					'ASSETS_LIABILITIES',
+					'CASH_DEBT',
+					'CASH_LIABILITIES',
+					'CURRENT_ASSETS_DEBT',
+					'CURRENT_ASSETS_LIABILITIES',
+					'EQUITY_DEBT',
+					'EQUITY_LIABILITIES',
+					'FCF_DEBT',
+					'FCF_LIABILITIES',
+					'FCF_SHY_DEBT',
+					'FCF_SHY_LIABILITIES',
+					'INCOME_DEBT',
+					'INCOME_LIABILITIES',
+					'TANGIBLE_DEBT',
+					'TANGIBLE_LIABILITIES'
+				)
 			])
 		)
 
 	def test_calculate_median_growth_rate(self):
 		growth_rates = analyst.calculate_moving_average_growth_rates(raw_data)
+		
 		self.assertEqual(
 			analyst.calculate_median_growth_rate(raw_data),
 			median([
-				growth_rates['GROWTH_NET_ASSETS'],
-				growth_rates['GROWTH_NET_CASH'],
-				growth_rates['GROWTH_NET_CASH_INVESTED'],
-				growth_rates['GROWTH_NET_EQUITY'],
-				growth_rates['GROWTH_NET_FCF'],
-				growth_rates['GROWTH_NET_FCF_SHY'],
-				growth_rates['GROWTH_NET_INVESTED_CAPITAL'],
-				growth_rates['GROWTH_NET_REVENUE'],
-				growth_rates['GROWTH_NET_TANGIBLE'],
+				growth_rates[f'GROWTH_NET_{column}'] for column in (
+					'ASSETS',
+					'CASH',
+					'CASH_INVESTED',
+					'EQUITY',
+					'FCF',
+					'FCF_SHY',
+					'INVESTED_CAPITAL',
+					'REVENUE',
+					'TANGIBLE',
+				)
 			])
 		)
 
 	def test_calculate_median_returns(self):
 		ratios = analyst.calculate_moving_average_ratios(raw_data)
+
 		self.assertEqual(
 			analyst.calculate_median_returns(raw_data),
 			median([
-				ratios['RATIO_INCOME_ASSETS'],
-				ratios['RATIO_INCOME_EQUITY'],
-				ratios['RATIO_INCOME_EXPENSES'],
-				ratios['RATIO_INCOME_INVESTED_CAPITAL'],
-				ratios['RATIO_INCOME_TANGIBLE'],
-				ratios['RATIO_FCF_ASSETS'],
-				ratios['RATIO_FCF_EQUITY'],
-				ratios['RATIO_FCF_EXPENSES'],
-				ratios['RATIO_FCF_INVESTED_CAPITAL'],
-				ratios['RATIO_FCF_TANGIBLE'],
-				ratios['RATIO_FCF_SHY_ASSETS'],
-				ratios['RATIO_FCF_SHY_EQUITY'],
-				ratios['RATIO_FCF_SHY_EXPENSES'],
-				ratios['RATIO_FCF_SHY_INVESTED_CAPITAL'],
-				ratios['RATIO_FCF_SHY_TANGIBLE'],
+				ratios[f'RATIO_{column}'] for column in (
+					'INCOME_ASSETS',
+					'INCOME_EQUITY',
+					'INCOME_EXPENSES',
+					'INCOME_INVESTED_CAPITAL',
+					'INCOME_TANGIBLE',
+					'FCF_ASSETS',
+					'FCF_EQUITY',
+					'FCF_EXPENSES',
+					'FCF_INVESTED_CAPITAL',
+					'FCF_TANGIBLE',
+					'FCF_SHY_ASSETS',
+					'FCF_SHY_EQUITY',
+					'FCF_SHY_EXPENSES',
+					'FCF_SHY_INVESTED_CAPITAL',
+					'FCF_SHY_TANGIBLE'
+				)
 			])
 		)
 
 	def test_calculate_median_safety(self):
 		safety = analyst.calculate_average_moving_average_differences(raw_data)
+		
 		self.assertEqual(
 			analyst.calculate_median_safety(raw_data),
 			median([
@@ -541,7 +674,9 @@ class TestStockPup(unittest.TestCase):
 				safety['DIFF_INCOME_LIABILITIES'],
 				safety['DIFF_TANGIBLE_DEBT'],
 				safety['DIFF_TANGIBLE_LIABILITIES'],
-				analyst.calculate_average_moving_averages(raw_data)['NET_WORKING_CAPITAL'],
+				analyst.calculate_average_moving_averages(
+					raw_data
+					)['NET_WORKING_CAPITAL'],
 			])
 		)
 
@@ -564,11 +699,37 @@ class TestStockPup(unittest.TestCase):
 			analyst.summarize(raw_data).index,
 			concat([
 				analyst.calculate_averages(raw_data),
-				analyst.calculate_average_moving_average_differences(raw_data),
-				analyst.calculate_moving_average_growth_rates(raw_data),
-				analyst.calculate_average_moving_averages(raw_data),
-				analyst.calculate_average_per_share_averages(raw_data),
-				analyst.calculate_average_per_share_differences(raw_data),
-				analyst.calculate_average_moving_average_ratios(raw_data),
+				(
+					analyst
+					.calculate_average_moving_average_differences(
+							raw_data
+						)
+				),
+				(
+					analyst
+						.calculate_moving_average_growth_rates(raw_data)
+				),
+				(analyst.calculate_average_moving_averages(
+						raw_data
+					)
+				),
+				(
+					analyst
+						.calculate_average_per_share_averages(
+							raw_data
+						)
+				),
+				(
+					analyst
+						.calculate_average_per_share_differences(
+							raw_data
+					)
+				),
+				(
+					analyst
+						.calculate_average_moving_average_ratios(
+							raw_data
+						)
+				),
 			], axis=0).index
 		)

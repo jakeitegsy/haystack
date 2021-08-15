50620e414c4bb1b6997e88685dd6fe64228f39b4
jakeitegsy@yahoo.com <jakeitegsy@yahoo.com>
2021-08-14 23:57:37 -0500

    Remove extraneous variables

--- tests/test_discount_cash_flows.py
+++ tests/test_discount_cash_flows.py
@@ -7,16 +7,27 @@ from numpy.testing import assert_equal
 class TestGetDiscountCashFlowValue(unittest.TestCase):
 
     def test_dcf_valuation_sums_each_cash_flow_using_discount_rate(self):
-        values = [1, 1.5, 1.2, 1.1]
         self.assertEqual(
-            round(Analyst().get_discount_cash_flow_value(values),2), 4.58
+            round(
+                Analyst().get_discount_cash_flow_value(
+                    [1, 1.5, 1.2, 1.1]
+                ),2
+            ), 
+            4.58
         )
 
     def test_dcf_valuation_when_nan_in_record(self):
-        values = [nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan,
-                  0., 0., -0.47, -0.47, -0.235, -7.43670886, -4.23944783]
         assert_equal(
-            round(Analyst().get_discount_cash_flow_value(values), 2),
+            round(
+                Analyst().get_discount_cash_flow_value(
+                    [
+                        nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, nan, 0., 0., 
+                        -0.47, -0.47, -0.235, -7.43670886, 
+                        -4.23944783
+                    ]
+                ), 
+                2
+            ),
             nan
         )
 
